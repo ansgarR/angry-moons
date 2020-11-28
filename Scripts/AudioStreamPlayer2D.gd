ebcf33f1d4ff8921869de0160e764b22f975a425
arenk@communicode.de <arenk@communicode.de>
2020-11-28 23:04:32 +0100

    switch intro to game music when intro ended

--- Scripts/AudioStreamPlayer2D.gd
+++ Scripts/AudioStreamPlayer2D.gd
@@ -5,10 +5,11 @@ var intro_music = load("res://Assets/Music/Music Intro.wav")
 var game_music = load("res://Assets/Music/Music Gameplay.wav")
 
 func _ready():
-	connect("intro_ended", self, "on_intro_ended")
+	intro_group.connect("intro_ended", self, "on_intro_ended")
 	set_stream(intro_music)
 
 func on_intro_ended():
-	print("caught intro end signal")
-	set_stream(game_music)
-	play()
+	print("caught intro end signal") #this control needs to be optimized
+	if game_music != get_stream():
+		set_stream(game_music)
+		play()
